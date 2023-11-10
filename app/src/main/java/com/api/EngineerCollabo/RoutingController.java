da40d04bb7b6510aa02fecdf10f524601b868402
aono <takeshi.aono001@gmail.com>
2023-11-10 22:47:51 +0900

    indexページの表示テスト

---
+++ app/src/main/java/com/api/EngineerCollabo/RoutingController.java
@@ -0,0 +1,49 @@
+package com.api.EngineerCollabo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("routing")
+public class RoutingController {
+
+    // routing/index の処理(GET)
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
+    public ModelAndView indexGet(ModelAndView model) {
+        // System.out.println("ID=" + id);
+        model.setViewName("indexc");
+        System.out.println("Hello");
+        System.out.println("Im fine");
+
+        return model;
+    }
+
+    // // routing/index の処理(POST)
+    // @RequestMapping(value = "/index", method = RequestMethod.POST)
+    // public Model indexPost(Model model, @ModelAttribute IndexForm form) {
+    //     System.out.println("form.name=" + form.name);
+
+    //     return model;
+    // }
+
+    // /**
+    //  * POST値保持用のフォームクラス
+    //  */
+    // public class IndexForm {
+    //     public String name ;
+
+    //     public String getName() {
+    //         return name;
+    //     }
+
+    //     public void setName(String name) {
+    //         this.name = name;
+    //     }
+
+    // }
+}
