37dfa290ca204dc3292ea27d0b83755c2471cea3
aono <takeshi.aono001@gmail.com>
2023-12-09 02:07:25 +0900

    シードデータ用実行ファイル作成

---
+++ app/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO projects (name, icon_url, description) VALUES ('test_project', 'test_icon_url', 'test_description');
+INSERT INTO users (is_owner, project_id, icon_url, name, introduce) VALUES (true, 1, 'test_url', 'test_user', 'test_introduce');
