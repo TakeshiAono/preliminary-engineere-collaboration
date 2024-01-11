4fcfc5784e46b0f609ac3db2b31a29e6b84ab6de
ayumi nakano <aynminoa@gmail.com>
2024-01-11 17:51:44 +0900

    passwordの文字数を100文字に変更

--- app/src/main/java/com/api/EngineerCollabo/entities/User.java
+++ app/src/main/java/com/api/EngineerCollabo/entities/User.java
@@ -33,7 +33,7 @@ public class User {
     @Column(name = "email", nullable = false, length = 50)
     private String email;
 
-    @Column(name = "password", nullable = false, length = 50)
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
     @Column(name = "iconUrl", nullable = true, length = 50)
