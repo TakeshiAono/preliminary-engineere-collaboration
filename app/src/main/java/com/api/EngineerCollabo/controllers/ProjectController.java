035dcaee331d3adc9e8b53954bdaca860a7ffdfe
aono <takeshi.aono001@gmail.com>
2024-02-18 15:32:46 +0900

    delete処理の実装

--- app/src/main/java/com/api/EngineerCollabo/controllers/ProjectController.java
+++ app/src/main/java/com/api/EngineerCollabo/controllers/ProjectController.java
@@ -3,6 +3,7 @@ package com.api.EngineerCollabo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -50,6 +51,14 @@ public class ProjectController {
                 project.setDescription(description);
             }
             projectRepository.save(project);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProject(@PathVariable("id") Optional<Integer> ID) {
+        if (ID.isPresent()) {
+            int id = ID.get();
+            projectRepository.deleteById(id);
         }
     }
 
