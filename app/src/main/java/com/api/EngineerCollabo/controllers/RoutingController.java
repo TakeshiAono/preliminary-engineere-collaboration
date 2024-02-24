b9c9f25a4a352d44726b399be558471bbccc2c0e
ayumi nakano <aynminoa@gmail.com>
2024-02-24 21:24:07 +0900

    import.sql修正、Use.javaに@ManyToManyの設定

--- app/src/main/java/com/api/EngineerCollabo/controllers/RoutingController.java
+++ app/src/main/java/com/api/EngineerCollabo/controllers/RoutingController.java
@@ -1,15 +1,10 @@
 package com.api.EngineerCollabo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 @RequestMapping("routing")
@@ -34,9 +29,9 @@ public class RoutingController {
         user_test.setEmail("testemail");
         user_test.setPassword("testpassword3");
 
-        user_test.setProject(project);
+        // user_test.setProject(project);
         userRepository.save(user_test);
-        
+
         return "indexc";
     }
 }
--- app/src/main/java/com/api/EngineerCollabo/entities/Project.java
+++ app/src/main/java/com/api/EngineerCollabo/entities/Project.java
@@ -1,22 +1,22 @@
 package com.api.EngineerCollabo;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Index;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
-import java.util.ArrayList;
-import java.util.List;
-import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
-@Table(name = "Projects", indexes = {@Index(name = "user_name_index", columnList = "name")})
+@Table(name = "Projects", indexes = { @Index(name = "user_name_index", columnList = "name") })
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,8 +31,8 @@ public class Project {
     @Column(name = "description", nullable = true, columnDefinition = "TEXT")
     private String description;
 
-    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
-    private List<User> users = new ArrayList<>();
+    // @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    // private List<User> users = new ArrayList<>();
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
     private List<Owner> owners = new ArrayList<>();
@@ -52,35 +52,35 @@ public class Project {
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
     private List<ChatRoom> chatRooms = new ArrayList<>();
 
-    public Integer getId() {
-        return id;
-    }
+    // public Integer getId() {
+    // return id;
+    // }
 
-    public void setId(Integer id) {
-        this.id = id;
-    }
+    // public void setId(Integer id) {
+    // this.id = id;
+    // }
 
-    public String getIconUrl() {
-        return iconUrl;
-    }
+    // public String getIconUrl() {
+    // return iconUrl;
+    // }
 
-    public void setIconUrl(String iconUrl) {
-        this.iconUrl = iconUrl;
-    }
+    // public void setIconUrl(String iconUrl) {
+    // this.iconUrl = iconUrl;
+    // }
 
-    public String getName() {
-        return name;
-    }
+    // public String getName() {
+    // return name;
+    // }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    // public void setName(String name) {
+    // this.name = name;
+    // }
 
-    public String getDescription() {
-        return description;
-    }
+    // public String getDescription() {
+    // return description;
+    // }
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+    // public void setDescription(String description) {
+    // this.description = description;
+    // }
 }
