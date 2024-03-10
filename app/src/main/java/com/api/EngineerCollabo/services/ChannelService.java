090127183b32d9e0381f908306cb6a5f0fec5fec
ayumi nakano <aynminoa@gmail.com>
2024-03-10 10:47:50 +0900

    createエラーのまま

--- app/src/main/java/com/api/EngineerCollabo/services/ChannelService.java
+++ app/src/main/java/com/api/EngineerCollabo/services/ChannelService.java
@@ -19,10 +19,38 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.EngineerCollabo.entities.Channel;
+import com.api.EngineerCollabo.entities.User;
+import com.api.EngineerCollabo.entities.ChatRoom;
 import com.api.EngineerCollabo.entities.ResponseChannel;
+import com.api.EngineerCollabo.repositories.ChatRoomRepository;
+import com.api.EngineerCollabo.repositories.UserRepository;
+import com.api.EngineerCollabo.repositories.ChannelRepository;
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ChannelService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ChatRoomRepository chatRoomRepository;
+
+    @Autowired
+    private ChannelRepository channelRepository;
+
+    public Channel createChannel(Integer userId, Integer chatRoomId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new EntityNotFoundException("ChatRoom not found"));
+
+        Channel channel = new Channel();
+        channel.setUser(user);
+        channel.setChatRoom(chatRoom);
+
+        return channelRepository.save(channel);
+    }
+
+
+
 
     public ResponseChannel changeResponseChannel(Channel channel) {
         ResponseChannel responseChannel = new ResponseChannel();
