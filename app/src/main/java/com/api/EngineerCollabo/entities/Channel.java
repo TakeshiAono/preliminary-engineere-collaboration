24cf0fc6dd2bc09297677e231efbd6d9533276cb
ayumi nakano <aynminoa@gmail.com>
2024-03-11 19:17:00 +0900

    import文やコードの整理

--- app/src/main/java/com/api/EngineerCollabo/entities/Channel.java
+++ app/src/main/java/com/api/EngineerCollabo/entities/Channel.java
@@ -1,18 +1,19 @@
 package com.api.EngineerCollabo.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import lombok.Data;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
@@ -27,7 +28,7 @@ public class Channel {
 
     @Column(name = "user_id")
     private Integer userId;
-    
+
     @Column(name = "chat_room_id")
     private Integer chatRoomId;
 
--- app/src/main/java/com/api/EngineerCollabo/services/ChannelService.java
+++ app/src/main/java/com/api/EngineerCollabo/services/ChannelService.java
@@ -1,30 +1,18 @@
 package com.api.EngineerCollabo.services;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Index;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
-import jakarta.persistence.PrePersist;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.EngineerCollabo.entities.Channel;
-import com.api.EngineerCollabo.entities.User;
 import com.api.EngineerCollabo.entities.ChatRoom;
 import com.api.EngineerCollabo.entities.ResponseChannel;
+import com.api.EngineerCollabo.entities.User;
+import com.api.EngineerCollabo.repositories.ChannelRepository;
 import com.api.EngineerCollabo.repositories.ChatRoomRepository;
 import com.api.EngineerCollabo.repositories.UserRepository;
-import com.api.EngineerCollabo.repositories.ChannelRepository;
+
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
@@ -40,7 +28,8 @@ public class ChannelService {
 
     public Channel createChannel(String name, Integer userId, Integer chatRoomId) {
         User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId).orElseThrow(() -> new EntityNotFoundException("ChatRoom not found"));
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                .orElseThrow(() -> new EntityNotFoundException("ChatRoom not found"));
         Channel channel = new Channel();
         channel.setName(name);
         channel.setUserId(user.getId());
@@ -49,9 +38,6 @@ public class ChannelService {
         return channelRepository.save(channel);
     }
 
-
-
-
     public ResponseChannel changeResponseChannel(Channel channel) {
         ResponseChannel responseChannel = new ResponseChannel();
         responseChannel.setId(channel.getId());
@@ -59,8 +45,7 @@ public class ChannelService {
         responseChannel.setUserId(channel.getUser().getId());
         responseChannel.setChatRoomId(channel.getChatRoom().getId());
         responseChannel.setMessageIds(
-            channel.getMessages().stream().map(message -> message.getId()).collect(Collectors.toList())
-    );
+                channel.getMessages().stream().map(message -> message.getId()).collect(Collectors.toList()));
         return responseChannel;
-      }
+    }
 }
