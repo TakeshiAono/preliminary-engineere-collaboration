e28251384ae524d0663eec71bdb0a1936c4e9548
ayumi nakano <aynminoa@gmail.com>
2024-03-12 19:39:59 +0900

    Get, Patch, Deleteを実装。

--- app/src/main/java/com/api/EngineerCollabo/entities/Message.java
+++ app/src/main/java/com/api/EngineerCollabo/entities/Message.java
@@ -1,17 +1,14 @@
 package com.api.EngineerCollabo.entities;
 
 import jakarta.persistence.Column;
-
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import lombok.Data;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Data
 @Entity
@@ -31,9 +28,10 @@ public class Message {
     @JoinColumn(name = "user_id", nullable = false, referencedColumnName = "id")
     private User user;
 
-    @ManyToOne()
-    @JoinColumn(name = "chat_room_id", nullable = false, referencedColumnName = "id")
-    private ChatRoom chatRoom;
+    // @ManyToOne()
+    // @JoinColumn(name = "chat_room_id", nullable = false, referencedColumnName =
+    // "id")
+    // private ChatRoom chatRoom;
 
     @ManyToOne()
     @JoinColumn(name = "channel_id", nullable = false, referencedColumnName = "id")
