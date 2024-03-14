f3761828c304638460cbdc47451c33ae673bb774
ayumi nakano <aynminoa@gmail.com>
2024-03-14 20:21:56 +0900

    patch, delete実装

--- app/src/main/java/com/api/EngineerCollabo/controllers/OfferController.java
+++ app/src/main/java/com/api/EngineerCollabo/controllers/OfferController.java
@@ -1,6 +1,11 @@
 package com.api.EngineerCollabo.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +37,29 @@ public class OfferController {
 
         if (userId != null && scoutedUserId != null) {
             offerService.createOffer(message, userId, scoutedUserId);
+        }
+    }
+
+    @PatchMapping("{id}")
+    public void putOffer(@PathVariable("id") Optional<Integer> ID, @RequestBody Offer requestOffer) {
+        if (ID.isPresent()) {
+            int id = ID.get();
+            Offer offer = offerRepository.findById(id);
+
+            String message = requestOffer.getMessage();
+            if (message != null) {
+                offer.setMessage(message);
+            }
+
+            offerRepository.save(offer);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public void deleteOffer(@PathVariable("id") Optional<Integer> ID) {
+        if (ID.isPresent()) {
+            int id = ID.get();
+            offerRepository.deleteById(id);
         }
     }
 
