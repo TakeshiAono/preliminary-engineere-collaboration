6b87e01e6e0dbb0b0c35e1db4e454835a07a3865
ayumi nakano <aynminoa@gmail.com>
2024-03-21 19:10:48 +0900

    Offerのread機能を実装

--- app/src/main/java/com/api/EngineerCollabo/controllers/OfferController.java
+++ app/src/main/java/com/api/EngineerCollabo/controllers/OfferController.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.api.EngineerCollabo.entities.Offer;
+import com.api.EngineerCollabo.entities.ResponseOffer;
 import com.api.EngineerCollabo.repositories.OfferRepository;
 import com.api.EngineerCollabo.repositories.UserRepository;
 import com.api.EngineerCollabo.services.OfferService;
@@ -37,6 +39,17 @@ public class OfferController {
 
         if (userId != null && scoutedUserId != null) {
             offerService.createOffer(message, userId, scoutedUserId);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseOffer responseOffer(@PathVariable("id") Optional<Integer> ID) {
+        if (ID.isPresent()) {
+            int id = ID.get();
+            Offer offer = offerRepository.findById(id);
+            return offerService.changResponseOffer(offer);
+        } else {
+            return null;
         }
     }
 
