6d1ebfd2e2e546217dacb0e7812172c5d2d041dd
ayumi nakano <aynminoa@gmail.com>
2024-03-23 08:43:03 +0900

    SkillのCRUD実装

---
+++ app/src/main/java/com/api/EngineerCollabo/entities/ResponseSkill.java
@@ -0,0 +1,15 @@
+package com.api.EngineerCollabo.entities;
+
+import lombok.Data;
+
+@Data
+public class ResponseSkill {
+    private Integer id;
+
+    private String name;
+
+    private Integer countLog;
+
+    private Integer UserId;
+
+}
