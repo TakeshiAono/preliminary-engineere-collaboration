afca6c21bb3013d090afac181dfb7a65c8223030
Takeshi　Aono <104053951+TakeshiAono@users.noreply.github.com>
2024-03-31 02:53:23 +0900

    Revert "[fixed] メッセージEntityにcreatedAtとupdatedAtを追加する"

--- app/src/main/java/com/api/EngineerCollabo/controllers/MessageController.java
+++ app/src/main/java/com/api/EngineerCollabo/controllers/MessageController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.api.EngineerCollabo.entities.Message;
 import com.api.EngineerCollabo.entities.ResponseMessage;
@@ -18,6 +19,7 @@ import com.api.EngineerCollabo.repositories.MessageRepository;
 import com.api.EngineerCollabo.services.MessageService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 @RequestMapping("/messages")
 public class MessageController {
     @Autowired
