2c6b56eab1c72636b7e3f4bf6a39e9c9cbd8beb7
ayumi nakano <aynminoa@gmail.com>
2024-04-25 17:37:38 +0900

    クライアント側からuserId, channelId, text, contentを送ってDBに保存できるよう実装

--- app/src/main/java/com/api/EngineerCollabo/services/MessageService.java
+++ app/src/main/java/com/api/EngineerCollabo/services/MessageService.java
@@ -1,90 +1,77 @@
-// package com.api.EngineerCollabo.services;
+package com.api.EngineerCollabo.services;
 
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-// import java.util.List;
-// import java.util.ArrayList;
-// import com.api.EngineerCollabo.entities.Channel;
-// import com.api.EngineerCollabo.entities.Message;
-// import com.api.EngineerCollabo.entities.ResponseMessage;
-// import com.api.EngineerCollabo.entities.User;
-// import com.api.EngineerCollabo.repositories.ChannelRepository;
-// import com.api.EngineerCollabo.repositories.MessageRepository;
-// import com.api.EngineerCollabo.repositories.UserRepository;
+import java.util.List;
+import java.util.ArrayList;
+import com.api.EngineerCollabo.entities.Channel;
+import com.api.EngineerCollabo.entities.Message;
+import com.api.EngineerCollabo.entities.ResponseMessage;
+import com.api.EngineerCollabo.entities.User;
+import com.api.EngineerCollabo.repositories.ChannelRepository;
+import com.api.EngineerCollabo.repositories.MessageRepository;
+import com.api.EngineerCollabo.repositories.UserRepository;
 
-// import jakarta.persistence.EntityNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 
-// @Service
-// public class MessageService {
+@Service
+public class MessageService {
 
-//     @Autowired
-//     private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final ChannelRepository channelRepository;
+    private final MessageRepository messageRepository;
 
-//     @Autowired
-//     private ChannelRepository channelRepository;
+    @Autowired
+    public MessageService(MessageRepository messageRepository, 
+                          UserRepository userRepository,
+                          ChannelRepository channelRepository) {
+        this.messageRepository = messageRepository;
+        this.userRepository = userRepository;
+        this.channelRepository = channelRepository;
+    }
 
-//     @Autowired
-//     private MessageRepository messageRepository;
+    public Message createMessage(String text, String content, Integer userId, Integer channelId) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
+        Channel channel = channelRepository.findById(channelId)
+                .orElseThrow(() -> new EntityNotFoundException("Channel not found"));
+        Message message = new Message();
+        message.setText(text);
+        message.setContent(content);
+        message.setUserId(user.getId());
+        message.setChannelId(channel.getId());
 
-//     @Autowired
-//     public MessageService(MessageRepository messageRepository) {
-//         this.messageRepository = messageRepository;
-//     }
+        return messageRepository.save(message);
+    }
 
-//     public void saveMessage(String text, String content, Integer userId, Integer channelId) {
-//         User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
-//         Channel channel = channelRepository.findById(channelId)
-//                 .orElseThrow(() -> new EntityNotFoundException("Channel not found"));
-//         Message message = new Message();
-//         message.setText(text);
-//         message.setContent(content);
-//         message.setUserId(user.getId());
-//         message.setChannelId(channel.getId());
-
-//         messageRepository.save(message);
-//     }
-
-//     public List<ResponseMessage> getAllMessages(){
-//         List<Message> messages = messageRepository.findAll();
-//         List<ResponseMessage> responseMessages = new ArrayList<>();
-//         for (Message message : messages){
-//             ResponseMessage responseMessage = new ResponseMessage();
-//             responseMessage.setId(message.getId());
-//             responseMessage.setText(message.getText());
-//             responseMessage.setContent(message.getContent());
-//             responseMessage.setUserId(message.getUser().getId());
-//             responseMessage.setChannelId(message.getChannel().getId());
-//             responseMessage.setCreatedAt(message.getCreatedAt());
-//             responseMessage.setUpdatedAt(message.getUpdatedAt());
-//             responseMessages.add(responseMessage);
-//         }
-//         return responseMessages;
-//     }
+    public List<ResponseMessage> getAllMessages(){
+        List<Message> messages = messageRepository.findAll();
+        List<ResponseMessage> responseMessages = new ArrayList<>();
+        for (Message message : messages){
+            ResponseMessage responseMessage = new ResponseMessage();
+            responseMessage.setId(message.getId());
+            responseMessage.setText(message.getText());
+            responseMessage.setContent(message.getContent());
+            responseMessage.setUserId(message.getUser().getId());
+            responseMessage.setChannelId(message.getChannel().getId());
+            responseMessage.setCreatedAt(message.getCreatedAt());
+            responseMessage.setUpdatedAt(message.getUpdatedAt());
+            responseMessages.add(responseMessage);
+        }
+        return responseMessages;
+    }
 
 
-//     // public Message createMessage(String text, String content, Integer userId, Integer channelId) {
-//     //     User user = userRepository.findById(userId).orElseThrow(() -> new EntityNotFoundException("User not found"));
-//     //     Channel channel = channelRepository.findById(channelId)
-//     //             .orElseThrow(() -> new EntityNotFoundException("Channel not found"));
-//     //     Message message = new Message();
-//     //     message.setText(text);
-//     //     message.setContent(content);
-//     //     message.setUserId(user.getId());
-//     //     message.setChannelId(channel.getId());
 
-//     //     return messageRepository.save(message);
-//     // }
-
-//     // public ResponseMessage changeResponseMessage(Message message) {
-//     //     ResponseMessage responseMessage = new ResponseMessage();
-//     //     responseMessage.setId(message.getId());
-//     //     responseMessage.setText(message.getText());
-//     //     responseMessage.setContent(message.getContent());
-//     //     responseMessage.setUserId(message.getUser().getId());
-//     //     responseMessage.setChannelId(message.getChannel().getId());
-//     //     responseMessage.setCreatedAt(message.getCreatedAt());
-//     //     responseMessage.setUpdatedAt(message.getUpdatedAt());
-//     //     return responseMessage;
-//     // }
-// }
+    // public ResponseMessage changeResponseMessage(Message message) {
+    //     ResponseMessage responseMessage = new ResponseMessage();
+    //     responseMessage.setId(message.getId());
+    //     responseMessage.setText(message.getText());
+    //     responseMessage.setContent(message.getContent());
+    //     responseMessage.setUserId(message.getUser().getId());
+    //     responseMessage.setChannelId(message.getChannel().getId());
+    //     responseMessage.setCreatedAt(message.getCreatedAt());
+    //     responseMessage.setUpdatedAt(message.getUpdatedAt());
+    //     return responseMessage;
+    // }
+}
