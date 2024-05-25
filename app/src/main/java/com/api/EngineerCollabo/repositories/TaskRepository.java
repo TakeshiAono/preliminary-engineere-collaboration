2ae11794fed3aeb296c65c8faad77f72c96bd738
TakeshiAono <takeshi.aono001@gmail.com>
2024-05-25 18:02:12 +0900

    未使用のimport文を削除

--- app/src/main/java/com/api/EngineerCollabo/repositories/TaskRepository.java
+++ app/src/main/java/com/api/EngineerCollabo/repositories/TaskRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.api.EngineerCollabo.entities.Project;
 import com.api.EngineerCollabo.entities.Task;
-import com.api.EngineerCollabo.entities.User;
 
 import java.util.List;
 
--- app/src/main/java/com/api/EngineerCollabo/services/ChatRoomService.java
+++ app/src/main/java/com/api/EngineerCollabo/services/ChatRoomService.java
@@ -1,31 +1,15 @@
 package com.api.EngineerCollabo.services;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Index;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.CascadeType;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 
-import jakarta.persistence.PrePersist;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.api.EngineerCollabo.entities.ChatRoom;
 import com.api.EngineerCollabo.entities.Project;
 import com.api.EngineerCollabo.entities.ResponseChatRoom;
-import com.api.EngineerCollabo.entities.User;
 import com.api.EngineerCollabo.repositories.ChatRoomRepository;
 import com.api.EngineerCollabo.repositories.ProjectRepository;
-import com.api.EngineerCollabo.repositories.UserRepository;
 
 import jakarta.persistence.EntityNotFoundException;
 
