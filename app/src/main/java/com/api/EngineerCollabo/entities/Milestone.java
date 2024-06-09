4b96628e50781fbf67483b49c3ca7c74d186daa9
calei <calei@078029@gmail.com>
2024-06-09 19:57:56 +0900

    #100 マイルストーンテーブルとAPIを作成する。

---
+++ app/src/main/java/com/api/EngineerCollabo/entities/Milestone.java
@@ -0,0 +1,56 @@
+package com.api.EngineerCollabo.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "Milestones")
+public class Milestone {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "deadline", nullable = true, columnDefinition = "DATE")
+    private Date deadline;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @OneToMany(mappedBy = "milestone", cascade = CascadeType.ALL)
+    @JsonManagedReference
+    private List<Task> tasks = new ArrayList<>();
+}
--- app/src/main/java/com/api/EngineerCollabo/initializer/TaskInitializer.java
+++ app/src/main/java/com/api/EngineerCollabo/initializer/TaskInitializer.java
@@ -30,21 +30,19 @@ public class TaskInitializer implements CommandLineRunner {
         try {
             for (int i = 1; i <= 100; i++) {
                 LocalDate startDate = LocalDate.of(2022, 1, 1); // 開始日
-                LocalDate endDate = LocalDate.of(2024, 1, 1);   // 終了日
+                LocalDate endDate = LocalDate.of(2024, 1, 1); // 終了日
                 Date randomDate = getRandomDate(startDate, endDate);
                 jdbcTemplate.update(
-                    "INSERT INTO tasks (in_charge_user_id, done_at, project_id, deadline, name, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    2, null, 1, randomDate, "バックエンド実装_", "バックエンドの実装をJavaで行う", randomDate, randomDate
-                );
+                        "INSERT INTO tasks (in_charge_user_id, done_at, project_id, deadline, milestone_id, name, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                        2, null, 1, randomDate, 1, "バックエンド実装_", "バックエンドの実装をJavaで行う", randomDate, randomDate);
             }
             for (int i = 1; i <= 100; i++) {
                 LocalDate startDate = LocalDate.of(2022, 1, 1); // 開始日
-                LocalDate endDate = LocalDate.of(2024, 1, 1);   // 終了日
+                LocalDate endDate = LocalDate.of(2024, 1, 1); // 終了日
                 Date randomDate = getRandomDate(startDate, endDate);
                 jdbcTemplate.update(
-                    "INSERT INTO tasks (in_charge_user_id, done_at, project_id, deadline, name, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-                    1, randomDate, 1, randomDate, "バックエンド実装_", "バックエンドの実装をJavaで行う", randomDate, randomDate
-                );
+                        "INSERT INTO tasks (in_charge_user_id, done_at, project_id, deadline, milestone_id, name, description, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                        1, randomDate, 1, randomDate, 2, "バックエンド実装_", "バックエンドの実装をJavaで行う", randomDate, randomDate);
             }
         } catch (Exception e) {
             System.out.println("Error during database operation: " + e.getMessage());
