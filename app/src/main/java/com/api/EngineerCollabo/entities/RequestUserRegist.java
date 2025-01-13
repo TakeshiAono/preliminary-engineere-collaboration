8e3643163e0d0e5ac0c21817a48b9dc8bdc4c67a
TakeshiAono <takeshi.aono001@gmail.com>
2025-01-13 12:10:29 +0900

    アカウント登録ができないバグを修正

--- app/src/main/java/com/api/EngineerCollabo/entities/RequestUserRegist.java
+++ app/src/main/java/com/api/EngineerCollabo/entities/RequestUserRegist.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class RequestUserRegist {
     // ユーザ名
-    // private String name;
+    private String name;
     // パスワード
     private String password;
     // メールアドレス
